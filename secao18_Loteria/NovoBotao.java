79514b34427f82ccfe1ebb51a9b60d1e97d06c79
emirfadul <emirfadul@gmail.com>
2025-02-02 00:26:19 -0400

    teste 2 commit

--- secao18_Loteria/NovoBotao.java
+++ secao18_Loteria/NovoBotao.java
@@ -4,6 +4,7 @@ public class NovoBotao {
 
     public static void main(String[] args) {
         System.out.println("teste novo branch github");
+        System.out.println("emir teste");
 
     }
 
