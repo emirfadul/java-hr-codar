23034c9c88700a344e295339c7f39cd17c85a1eb
Emir <emir@emirfaul.localdomain>
2025-02-16 11:38:16 -0400

    Atualizado em 16.fev.2025 as 11:40

---
+++ secao22/Arquivo.java
@@ -0,0 +1,19 @@
+package secao22;
+
+public class Arquivo {
+
+    public static void main(String[] args) {
+
+        //Leitura arquivo
+        System.out.println("------- Leitura Arquivo ------");
+
+        String currentDir = System.getProperty("user.dir")+"/secao22/";
+
+        System.out.println(currentDir+"arquivo.txt");
+        System.out.println("/home/emir/java/hora_de_codar/secao22");
+
+        
+
+    }
+    
+}
