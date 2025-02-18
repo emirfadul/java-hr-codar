694a5572abdc4a7fb8a64924c5e13de528a3869d
Emir <emir@emirfaul.localdomain>
2025-02-18 11:39:59 -0400

    Atualizado em 18/02/2025 as 11:41

--- secao23/Generics.java
+++ secao23/Generics.java
@@ -1,5 +1,8 @@
 package secao23;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
 
     public static void main(String[] args) {
@@ -37,8 +40,29 @@ public class Generics {
 
         Comparador<Integer> comparadorInteiros = new Comparador<>();
 
-        System.out.println("Maior numero entre 20 e 10: "+comparadorInteiros.obterMaior(20, 10));        
+        System.out.println("Maior numero entre 20 e 10: "+comparadorInteiros.obterMaior(20, 10));    
         
+        //Wildcards
+        System.out.println("------ Wildcard -------");
+
+        List<Integer> numero2 = List.of(1,2,3);
+        List<String> palavras2 = List.of("teste", "java", "ola");
+
+        //aceita qualquer tipo de lista(?)
+        imprimirLista(numero2);
+        imprimirLista(palavras2);  
+        
+        //aceita listas Numericas (? extends Number)
+        System.out.println(somarNumeros(numero2));
+
+        // aceita apenas tipos numericos especificos
+        List<Number> numeros3 = new ArrayList<>();
+
+        adicionarNumeros(numeros3);
+
+        for (Number numero : numeros3) {
+            System.out.println(numero);    
+        }
     }
 
     public static <T extends Comparable<T>> T obterMaior(T valor1, T valor2){
@@ -50,6 +74,32 @@ public class Generics {
             System.out.print(elemento+", ");                        
         }
         System.out.println(" ");
-
     }
+
+    public static void imprimirLista(List<?> lista){
+        for (Object elemento : lista) {
+            System.out.println(elemento);            
+        }
+    }
+
+    public static double somarNumeros(List<? extends Number> lista){
+        double soma = 0;
+
+        for (Number numero : lista) {
+            soma += numero.doubleValue();
+        }
+        return soma;
+    }
+
+    public static void adicionarNumeros(List<? super Integer> lista){
+        for (int i = 0; i <= 5; i++) {
+            lista.add(i);            
+        }
+    }
+
+
+
+
+
+
 }
