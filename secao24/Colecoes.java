6ea7de6870ea40a86bbe379d01cb41a3ea5211dc
Emir <emir@emirfaul.localdomain>
2025-02-21 10:22:12 -0400

    Atualizado em 21/02/2025 as 10:24

--- secao24/Colecoes.java
+++ secao24/Colecoes.java
@@ -1,13 +1,18 @@
 package secao24;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Colecoes {
 
     public static void main(String[] args) {
         
-        //List adidionando
+        //ArrayList adidionando
         System.out.println("------ LIST -----");
         List<String> listaDeNomes = new ArrayList<>();
 
@@ -46,6 +51,65 @@ public class Colecoes {
 
         //pesquisando no array
         System.out.println("Existe o nome ANA: "+listaDeNomes.contains("ana"));
+
+        //linkedlist
+        System.out.println("----- LINKEDLIST ------");
+        List<Integer> numeros = new LinkedList<>();
+
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+
+        System.out.println("O Linkedlist é "+numeros.toString());
+        System.out.println("O segundo indice é: "+numeros.get(2));
+
+        //Set - HashSet (ordena os numeros)
+        System.out.println("----- Set com HashSet------");
+
+        Set<String> conjunto = new HashSet<>();
+
+        conjunto.add("python");
+        conjunto.add("PHP");
+        conjunto.add("JAVA");
+        conjunto.add("HTML");
+
+        System.out.println(conjunto);
+
+        //LinkedHashSet
+        System.out.println("----- LinkedHashSet ------");
+
+        Set<Integer> numeros2 = new LinkedHashSet<>();
+
+        numeros2.add(10);
+        numeros2.add(50);
+        numeros2.add(30);
+        numeros2.add(20);
+
+        System.out.println(numeros2);
+
+        //TreeSet - ordena os itens
+        System.out.println("----- TreeSet ------");
+
+        Set<String> nomes = new TreeSet<>();
+
+        nomes.add("emir");
+        nomes.add("joao");
+        nomes.add("viana");
+        nomes.add("alan");
+
+
+        System.out.println(nomes);
+
+
+
+
+
+
+
+        
+        
+
              
 
 
