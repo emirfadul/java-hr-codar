8914218067c25fc06206a81a61f0030bb5a8cadb
emirfadul <emirfadul@gmail.com>
2025-03-11 16:54:22 -0400

    Atualizado 07.03.2025 as 16h54

---
+++ secao26/Animal.java
@@ -0,0 +1,14 @@
+package secao26;
+
+public class Animal {
+
+    public void emitirSom(){
+        System.out.println("O animal está emitindo som.");
+    }
+
+    @Deprecated
+    public void mover(){
+        System.out.println("O animal está se movendo.");;
+    }
+    
+}
---
+++ secao26/Anotacoes.java
@@ -0,0 +1,17 @@
+package secao26;
+
+public class Anotacoes {
+    
+    public static void main(String[] args) {
+        
+        //anotation pre definidas
+
+        Cachorro cachorro = new Cachorro();
+
+        cachorro.emitirSom();
+        cachorro.testeMover();
+        //cachorro.mover();
+
+
+    }
+}
