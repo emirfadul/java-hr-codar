ad528ba784a58be217bfbfe2485a8797c5689d39
Emir <emir@emirfaul.localdomain>
2025-03-24 11:38:30 -0400

    atualizado 23/05/2025 as 11:41

---
+++ secao27/HelloWorldFX/src/helloworldfx/ButtonLabelExample.java
@@ -0,0 +1,40 @@
+package helloworldfx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ButtonLabelExample extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        //criar label
+        Label label = new Label("Este é um label");
+
+        Button button = new Button("clicar");
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(label,button);
+        button.setOnAction(event -> label.setText("Clicou no botao"));
+        
+        //criando cena com layout
+        Scene scene = new Scene(vbox,300,300);
+        
+        // configuração do stage
+        primaryStage.setTitle("Exemplo GridPane");
+        primaryStage.setScene(scene);
+        primaryStage.show();        
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
