8ec2e4e5781193a118d329e7f62c193b0204be97
Emir <emir@emirfaul.localdomain>
2025-03-26 01:02:15 -0400

    Atualizado em 26/03/2025 as 1:05

--- secao27/HelloWorldFX/src/helloworldfx/AlertExample.java
+++ secao27/HelloWorldFX/src/helloworldfx/AlertExample.java
@@ -1,43 +1,36 @@
 package helloworldfx;
 
-import javafx.application.Application;
+import javafx.application.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
-public class Alert extends Application{
+public class AlertExample extends Application{
     
     @Override
     public void start(Stage primaryStage) throws Exception{  
 
         Button buttonInfo = new Button("Alerta de info");
-
         buttonInfo.setOnAction(event -> showAlert(AlertType.INFORMATION,"Informação", "Alerta de informação"));
 
-        VBox vbox = new VBox(10);
+        Button buttonError = new Button("Alerta de error");
+        buttonError.setOnAction(event -> showAlert(AlertType.ERROR,"ERRO", "Alerta de erro"));
 
-        vbox.getChildren().addAll(buttonInfo);
+        Button buttonWarning = new Button("Alerta de warning");
+        buttonWarning.setOnAction(event -> showAlert(AlertType.WARNING,"Aviso", "Alerta de aviso"));
+
+        Button buttonConfirmation = new Button("Alerta de confirmação");
+        buttonConfirmation.setOnAction(event -> showAlert(AlertType.CONFIRMATION,"confirmação", "Alerta de confirmação"));
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(buttonInfo,buttonError,buttonWarning,buttonConfirmation);
 
         //config do stage
         Scene scene = new Scene(vbox,500,500);
-
                 
         // configuração do stage
         primaryStage.setTitle("Exemplo de Alerta");
@@ -45,7 +38,8 @@ public class Alert extends Application{
         primaryStage.show();        
     }
 
-    private void showAlert(AlertType alertType, String title, String message){
+    private void showAlert(AlertType alertType, String title, String message) {
+
         Alert alert = new Alert(alertType);
 
         alert.setTitle(title);
@@ -53,7 +47,6 @@ public class Alert extends Application{
         alert.setContentText(message);
 
         alert.showAndWait();
-
     }
 
 
