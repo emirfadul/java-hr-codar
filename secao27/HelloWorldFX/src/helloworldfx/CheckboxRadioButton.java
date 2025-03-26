2892b0920b80938edd7f449fcdc54cc5fa672880
Emir <emir@emirfaul.localdomain>
2025-03-25 20:25:26 -0400

    Atualizado em 25 março as 20:28

--- secao27/HelloWorldFX/src/helloworldfx/CheckboxRadioButton.java
+++ secao27/HelloWorldFX/src/helloworldfx/CheckboxRadioButton.java
@@ -5,8 +5,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -27,9 +29,27 @@ public class CheckboxRadioButton extends Application{
         checkbox1.setOnAction(event -> updateCheckboxLabel(checkbox1,checkbox2,checkboxLabel));
         checkbox2.setOnAction(event -> updateCheckboxLabel(checkbox1,checkbox2,checkboxLabel));
 
+        //radio button
+        RadioButton radioButton1 = new RadioButton("Opção A");
+        RadioButton radioButton2 = new RadioButton("Opção B");
 
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        radioButton1.setToggleGroup(toggleGroup);
+        radioButton2.setToggleGroup(toggleGroup);
+
+        Label radioLabel = new Label("Radio button selecionado: ");
+
+        toggleGroup.selectedToggleProperty().addListener((observable,oldValue,newValue) -> {
+            if (newValue != null) {
+                RadioButton selectedRadioButton = (RadioButton) toggleGroup.getSelectedToggle();
+
+                radioLabel.setText("Radio button selecionado: " + selectedRadioButton.getText());
+            }
+        });
+        
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(checkboxLabel,checkbox1,checkbox2);
+        vbox.getChildren().addAll(checkboxLabel,checkbox1,checkbox2,radioLabel,radioButton1,radioButton2);
             
         //criando cena com layout
         Scene scene = new Scene(vbox,300,300);
@@ -48,7 +68,6 @@ public class CheckboxRadioButton extends Application{
         if(cb2.isSelected()) selected += cb2.getText();
 
         label.setText(selected);
-
     }
 
 
